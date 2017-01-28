3705350efd66c0f70001ff0ffaf6d8fd5ffd6631
poetienshul <ethantien@hotmail.com>
2017-01-28 18:27:20 -0500

    Add readme.p3.txt and delete useless.p3.txt, changed Person3.java and updated AntLabMain.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		String str = "";
+		for (int i = str.length() - 1; i >= 0; i--) {
+			str += input.charAt(i);
+		}
+	  	return str;
 	}
 }
