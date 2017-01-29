366b333d30133c760946cc260fec15665d10af94
cfeng46 <cfeng7@crimson.ua.edu>
2017-01-29 00:52:32 -0500

    gradle

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -31,7 +31,13 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return input;
+		String ans = "";
+		for (int i = 2; i < input.length(); i++) {
+			ans = ans + input.charAt(i);
+		}
+        ans = ans + input.charAt(0);
+        ans = ans + input.charAt(1);
+	    return ans;
 	}
 	
 	/**
